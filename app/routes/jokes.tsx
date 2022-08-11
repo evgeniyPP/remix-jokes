e2d72cc42fb6f6f51d33d1b4189b57a3ed72bb61
Eugene Petryankin <epetryankin@devvela.com>
2022-08-12 01:13:34 +0300

    Implement logout

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,26 +1,30 @@
 import type { LinksFunction, LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Outlet, Link, useLoaderData } from '@remix-run/react';
+import type { Joke } from '@prisma/client';
 
 import { db } from '~/utils/db.server';
-import type { Joke } from '@prisma/client';
+import { getUser } from '~/utils/session.server';
 import stylesUrl from '~/styles/jokes.css';
 
-type LoaderData = { jokeListItems: Pick<Joke, 'id' | 'name'>[] };
+type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>;
+  jokeListItems: Pick<Joke, 'id' | 'name'>[];
+};
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }];
 };
 
-export const loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: 'desc' }
-    })
-  };
+export const loader: LoaderFunction = async ({ request }) => {
+  const user = await getUser(request);
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    select: { id: true, name: true },
+    orderBy: { createdAt: 'desc' }
+  });
 
+  const data: LoaderData = { user, jokeListItems };
   return json(data);
 };
 
@@ -37,6 +41,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">

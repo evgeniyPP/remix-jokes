fdaf64282a56584847b7131249a1755483425ebf
Eugene Petryankin <epetryankin@devvela.com>
2022-08-12 01:16:47 +0300

    Implement register

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,7 +3,7 @@ import { json } from '@remix-run/node';
 import { useActionData, Link, useSearchParams } from '@remix-run/react';
 
 import { db } from '~/utils/db.server';
-import { createUserSession, login } from '~/utils/session.server';
+import { createUserSession, login, register } from '~/utils/session.server';
 import stylesUrl from '~/styles/login.css';
 
 export const links: LinksFunction = () => {
@@ -95,18 +95,18 @@ export const action: ActionFunction = async ({ request }) => {
         });
       }
 
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: 'Not implemented'
-      });
+      const user = await register({ username, password });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Something went wrong trying to create a new user.`
+        });
+      }
+
+      return createUserSession(user.id, redirectTo);
     }
     default: {
-      return badRequest({
-        fields,
-        formError: `Login type invalid`
-      });
+      return badRequest({ fields, formError: `Login type invalid` });
     }
   }
 };

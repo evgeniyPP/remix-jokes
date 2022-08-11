415d22a61298c0038aa405013118361bf8bb3e32
Eugene Petryankin <epetryankin@devvela.com>
2022-08-11 14:08:08 +0300

    Add mutation

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,23 @@
+import type { ActionFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+
+import { db } from '~/utils/db.server';
+
+export let action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  if (typeof name !== 'string' || typeof content !== 'string') {
+    throw new Error('Invalid form data');
+  }
+
+  const joke = await db.joke.create({
+    data: { name, content }
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
